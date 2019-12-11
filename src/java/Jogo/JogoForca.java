aee911f84a0e6648a9aeeba8854bfecd7ef0bb79
Marcos Reis <marcossummers@gmail.com>
2019-12-10 23:07:42 -0300

    Começando a atividade

---
+++ src/java/Jogo/JogoForca.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jogo;
+
+import Models.Carros;
+import Models.Filme;
+import Models.Novela;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author marco
+ */
+public class JogoForca extends HttpServlet
+{
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+}
---
+++ src/java/Models/Novela.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author marco
+ */
+public class Novela {
+    
+    String[] titulos = {"Por Amor","Roque Santeiro","A Próxima Vítima","Laços de Familia"};
+}
